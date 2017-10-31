dc8079abcfdc8bf2812b0cc73d079d1dcca5d2dc
Geoffry Berryman <geoffry.berryman@gmail.com>
2017-10-31 12:48:29 -0400

    Updating Homework

--- homework1.py
+++ homework1.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #Geoffry Berryman DSCI 401 Homework 1
 
 def flatten(y):
@@ -40,49 +39,9 @@ def  numspi(num, d):
 	my_list = []
 	for item in nums:
 		my_list.append(item)
-	while(count < num)
+	while(count < num):
 		for item in my_list:
-			
-		
+			my_list = my_list.extend(numspi(item,d))
+	print('I have no clue what Im doing :) ')	
 	return(my_list)	
-=======
-#Geoffry Berryman DSCI 401 Homework 1
-
-def flatten(y):
-  answer = []
-  for item in y:
-    if type(item) == type([]):
-      answer.extend(flatten(item))
-    else:
-	  answer.append(item)
-  return answer
-  
-def powerset(z):
-	the_set = [[]]
-	count = 1
-	for item in z:
-		for thing in the_set:
-			the_set = the_set + [list(thing)+[item]]
-			count = count+1
-	print ("Number of combinations: ",count)
-	return the_set
-
-#def all_perms(x):
-
-def all_perms(z):
-    if not z:
-            return [[]]
-    answer = []
-    for thing in z:
-            temp = z[:]
-            temp.remove(thing)
-            answer.extend([[thing] + a for a in all_perms(temp)])
-    return answer
-	
-		
-def  numspi(num, squ):
-	max = num*num
-	size = num
-	nums = list(range(0,max))
->>>>>>> b235cb476479d9da1631f2ebdfc4341205116d93
 	
