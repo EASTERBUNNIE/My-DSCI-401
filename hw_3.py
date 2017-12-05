dd9702b5ca4b761e7c4cc2af38b9fd559194c179
EASTERBUNNIE <gberryma@mail.umw.edu>
2017-12-05 13:13:36 -0500

    Updating to reflect new method

--- hw_3.py
+++ hw_3.py
@@ -25,6 +25,9 @@ cols2 = list(TEST.columns.values)#Same except for test data
 cols2.pop(cols2.index('Churn')) 
 TEST = TEST[['Churn']+cols2]
 
+#del df['Income']
+#del TEST['Income']
+
 def cat_features(dataframe):
 	td = pd.DataFrame({'a':[1,2,3], 'b':[1.0, 2.0, 3.0]})
 	return filter(lambda x: not(dataframe[x].dtype in [td['a'].dtype, td['b'].dtype]), list(dataframe))
@@ -46,14 +49,6 @@ del TEST2['CustID']
 del df['CustID']
 del TEST2['Churn_No']
 
-#del df['Visits']
-#del df['Calls']
-del df['Age']
-del TEST2['Age']
-#del TEST2['Calls']
-#del TEST2['Visits']
-
-
 data_y = df['Churn_Yes']
 del df['Churn_Yes']
 data_x = df
@@ -61,31 +56,49 @@ test_y = TEST2['Churn_Yes']
 del TEST2['Churn_Yes']
 test_x = TEST2
 
-x_train, x_test, y_train, y_test = train_test_split(data_x, data_y, test_size = 0.15, random_state = 4)
+the_set = [[]]
+num = 0	
+prec = 0
+knum = 0
+my_list = []
+for item in list(data_x):
+	for thing in the_set:
+		the_set = the_set + [list(thing)+[item]]
+		data_x = pd.DataFrame(data=df, columns=thing)
+		x_train, x_test, y_train, y_test = train_test_split(data_x, data_y, test_size = 0.2, random_state = 4)
+		ks = [2, 3, 4, 5, 6, 7, 8,9,10,12,14,16]
+		if len(thing)>0:
+			for k in ks:
+				mod = neighbors.KNeighborsClassifier(n_neighbors=k)
+				mod.fit(x_train, y_train)
+				preds = mod.predict(x_test)
+				if str(accuracy_score(y_test, preds)) > num:
+					if str(precision_score(y_test, preds)) > prec:
+						prec = str(precision_score(y_test, preds))
+						num = str(accuracy_score(y_test, preds))
+						knum = k
+						conmat = str(confusion_matrix(y_test, preds))
+						my_list = list(thing)
+print('Number of Neighbors: '+str(knum))
+print('For: '+str(my_list))
+print('Accuracy: '+str(num))
+print('Precision: '+str(prec))
+print("Confusion Matrix:\n" + str(conmat))
 
-	
-ks = [2, 3, 4, 5, 6, 7, 8,9,10,12,14,16]
-for k in ks:
-	mod = neighbors.KNeighborsClassifier(n_neighbors=k)
-	mod.fit(x_train, y_train)
-	preds = mod.predict(x_test)
-	print('---------- EVALUATING TRAINING MODEL: k = ' + str(k) + ' -------------------')
-	print('Accuracy: ' + str(accuracy_score(y_test, preds)))
-	print('Precison: ' + str(precision_score(y_test, preds)))
-	print('Recall: ' + str(recall_score(y_test, preds)))
-	print('F1: ' + str(f1_score(y_test, preds)))
-	print("Confusion Matrix:\n" + str(confusion_matrix(y_test, preds)))
-	
-ks = [8]
-for k in ks:
-	mod = neighbors.KNeighborsClassifier(n_neighbors=k)
-	mod.fit(data_x, data_y)
-	preds = mod.predict(test_x)
-	print('---------- TESTING MODEL: k = ' + str(k) + ' -------------------')
-	print('Accuracy: ' + str(accuracy_score(test_y, preds)))
-	print('Precison: ' + str(precision_score(test_y, preds)))
-	print('Recall: ' + str(recall_score(test_y, preds)))
-	print('F1: ' + str(f1_score(test_y, preds)))
-	print("Confusion Matrix:\n" + str(confusion_matrix(test_y, preds)))
+#Test on Validation data now that we have best KNN
+data_x = pd.DataFrame(df, columns=my_list)
+valid = pd.DataFrame(TEST2, columns=my_list)
+x_train, x_test, y_train, y_test = train_test_split(data_x, data_y, test_size = 0.2, random_state = 4)
+mod=neighbors.KNeighborsClassifier(n_neighbors=knum)
+mod.fit(x_train, y_train)
+preds = mod.predict(valid)
+print("-----------------Testing Churn Validation-------------------")
+print('Accuracy: ' + str(accuracy_score(test_y, preds)))
+print('Precison: ' + str(precision_score(test_y, preds)))
+print('Recall: ' + str(recall_score(test_y, preds)))
+print('F1: ' + str(f1_score(test_y, preds)))
+print("Confusion Matrix:\n" + str(confusion_matrix(test_y, preds)))
+
+
 
 
